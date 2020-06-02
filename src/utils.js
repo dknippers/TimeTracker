112037da59a0cda8534c0128a9af000ac59f1949
Daniël Knippers <dknippers@gmail.com>
2020-06-02 16:47:58 +0200

    Fixes regression where time cannot be entered without ":" between hr/min
    This was the original behavior but the ":" was made optional in an earlier
    release, and was lost when refactoring to a Vue SFC. This has now been restored.

--- src/utils.js
+++ src/utils.js
@@ -105,8 +105,10 @@ function pad(input, char, length) {
  * @param {string} time Time string to convert to a timestamp.
  */
 function timeToTimestamp(time) {
-  const timeRe = /^[012]?[0-9]:[0-5][0-9]$/;
-  if (!timeRe.test(time)) {
+  const timeRe = /^([012]?[0-9]):?([0-5][0-9])$/;
+  const matches = timeRe.exec(time);
+
+  if (matches == null || matches.length !== 3) {
     if (time) {
       console.warn(`Invalid time string, should match RegExp ${timeRe} but got "${time}"`);
     }
@@ -114,9 +116,8 @@ function timeToTimestamp(time) {
     return null;
   }
 
-  var hrMin = time.split(":").map(v => parseInt(v));
-  const hr = hrMin[0];
-  const min = hrMin[1];
+  const hr = parseInt(matches[1], 10);
+  const min = parseInt(matches[2], 10);
 
   const dt = new Date();
   dt.setHours(hr);
